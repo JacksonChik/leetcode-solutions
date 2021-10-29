432197d3e238ef40428162a43df62f4c23d47c03
Chik Jackson <Jacksonchik01@hotmail.com>
2021-10-29 16:49:56 -0700

    Added evaluation on FBU OA(on 2021.10.28) to lc notes

--- q3LongestSubstringWithoutRepeatingCharacters.java
+++ q3LongestSubstringWithoutRepeatingCharacters.java
@@ -1,27 +1,38 @@
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
-    int rightPtr = 0;
-    int leftPtr = 0;
-    int globalMax = 0;
-    Map<Character,Integer> charLastIndex = new HashMap<Character,Integer>();
-        for(int i = 0; i < s.length(); i++){
-            Character currentCharacter = new Character(s.charAt(i));
-            if(charLastIndex.containsKey(currentCharacter)){
-                rightPtr = i;
-                leftPtr = charLastIndex.get(currentCharacter);
-                
-                charLastIndex.clear(); 
-                
-                for(int j = leftPtr; j <= rightPtr; j++){
-                    charLastIndex.put(Character.valueOf(s.charAt(j)), j);
-                }
-
-                continue;
-            }
-        charLastIndex.put(currentCharacter, i);
-        rightPtr++;
-        globalMax = (rightPtr - leftPtr) > globalMax ? rightPtr - leftPtr : globalMax;
+    public int maxArea(int[] height) {
+        int max = 0;
+        int ptrLeft = 0;
+        int ptrRight = height.length-1;
+        
+        while(ptrRight > ptrLeft){
+            int currentArea = (ptrRight - ptrLeft) * Math.min(height[ptrRight], height[ptrLeft]);
+            max = Math.max(currentArea, max);
+            
+            if (height[ptrLeft] >= height[ptrRight]) ptrRight--;
+            // If this evaluates to true, the value of Math.min(height[ptrLeft], height[ptrRight])
+            // can still be increased, which means that the area can POSSIBLY be INCREASED(with the              // current ptrLeft) by moving ptrRight leftwards!
+            //TRICKY STUFF            
+            else ptrLeft++;
+            
         }
-    return globalMax;
+        return max;
     }
 }
+
+// NON-OPTIMAL IMPLEMENTATION
+// class Solution {
+//     public int maxArea(int[] height) {
+//         int max = 0;
+//        for(int ptrFast = 1, ptrSlow =0; ptrFast < height.length; ptrFast++){
+//            int currentArea = Math.min(height[ptrFast], height[ptrSlow]) * (ptrFast - ptrSlow);
+//            for(int j = 0; j < ptrFast; j++){
+//                if((Math.min(height[ptrFast], height[j]) * (ptrFast - j)) > currentArea){
+//                    currentArea = (Math.min(height[ptrFast], height[j]) * (ptrFast - j));
+//                    ptrSlow = j;
+//                }
+//            }
+//            if(currentArea > max) max = currentArea;
+//        }
+//         return max;
+//     }
+// }
