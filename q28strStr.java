9abcefcc74932b95b6aac6b57205b99a7034048f
Chik Jackson <Jacksonchik01@hotmail.com>
2022-05-11 13:16:13 -0700

    solved q28

---
+++ q28strStr.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        for(int i = 0; i < haystack.length(); i++){
+            if(haystack.charAt(i) == needle.charAt(0)){
+                for(int j = 0; j < needle.length() && i + j < haystack.length(); j++){
+                    if(haystack.charAt(i + j) != needle.charAt(j)) break;
+                    if(j == needle.length()-1) return i;
+                }
+            }
+        }
+                return -1;
+    }
+    
+}
