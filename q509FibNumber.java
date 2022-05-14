c4d6abc74c6caf38c5825d40e2ce78740a4d41a4
Chik Jackson <Jacksonchik01@hotmail.com>
2022-05-14 16:23:06 -0700

    solved q509

---
+++ q509FibNumber.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        int[] result = new int[n+1];
+        result[0] = 0;
+        result[1] = 1;
+            for(int i = 2; i < n+1; i++){
+                result[i] = result[i - 1] + result[i-2];
+            }
+        return result[n];
+    }
+}
