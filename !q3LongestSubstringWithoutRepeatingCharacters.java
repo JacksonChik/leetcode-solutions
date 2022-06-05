deed7bb996193962c96f8847461db6803aab387c
Chik Jackson <Jacksonchik01@hotmail.com>
2022-06-05 10:39:44 -0400

    a more elegant solution for q3 with sliding window algo

--- !q3LongestSubstringWithoutRepeatingCharacters.java
+++ !q3LongestSubstringWithoutRepeatingCharacters.java
@@ -1,38 +1,21 @@
 class Solution {
-    public int maxArea(int[] height) {
-        int max = 0;
-        int ptrLeft = 0;
-        int ptrRight = height.length-1;
-        
-        while(ptrRight > ptrLeft){
-            int currentArea = (ptrRight - ptrLeft) * Math.min(height[ptrRight], height[ptrLeft]);
-            max = Math.max(currentArea, max);
-            
-            if (height[ptrLeft] >= height[ptrRight]) ptrRight--;
-            // If this evaluates to true, the value of Math.min(height[ptrLeft], height[ptrRight])
-            // can still be increased, which means that the area can POSSIBLY be INCREASED(with the              // current ptrLeft) by moving ptrRight leftwards!
-            //TRICKY STUFF            
-            else ptrLeft++;
-            
+    public int lengthOfLongestSubstring(String s) {
+        int maxLen = 0;
+        for(int i = 0, j = 0; j < s.length(); j++){
+            while(Solution.containsDuplicate(s.substring(i,j + 1)) && i <= j){
+                i++;
+            }
+            maxLen = j - i + 1 > maxLen ? j - i + 1: maxLen;
         }
-        return max;
+        return maxLen;
+    }
+    
+    static public boolean containsDuplicate(String s){
+        HashSet<Character> usedChar = new HashSet<Character>();
+        for(int i = 0; i < s.length(); i++){
+            if(usedChar.contains(s.charAt(i))) return true;
+            usedChar.add(s.charAt(i));
+        }
+        return false;
     }
 }
-
-// NON-OPTIMAL IMPLEMENTATION
-// class Solution {
-//     public int maxArea(int[] height) {
-//         int max = 0;
-//        for(int ptrFast = 1, ptrSlow =0; ptrFast < height.length; ptrFast++){
-//            int currentArea = Math.min(height[ptrFast], height[ptrSlow]) * (ptrFast - ptrSlow);
-//            for(int j = 0; j < ptrFast; j++){
-//                if((Math.min(height[ptrFast], height[j]) * (ptrFast - j)) > currentArea){
-//                    currentArea = (Math.min(height[ptrFast], height[j]) * (ptrFast - j));
-//                    ptrSlow = j;
-//                }
-//            }
-//            if(currentArea > max) max = currentArea;
-//        }
-//         return max;
-//     }
-// }
